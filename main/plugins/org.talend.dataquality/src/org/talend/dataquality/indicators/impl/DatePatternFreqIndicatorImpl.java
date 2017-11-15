55879f36fac012baa030858e1bb55cdf7a32a23a
msjian <msjian@talend.com>
2017-11-15 16:47:44 +0800

    TDQ-14467: fix by make the Generate Regex Pattern more exactly correct

--- main/plugins/org.talend.dataquality/src/org/talend/dataquality/indicators/impl/DatePatternFreqIndicatorImpl.java
+++ main/plugins/org.talend.dataquality/src/org/talend/dataquality/indicators/impl/DatePatternFreqIndicatorImpl.java
@@ -48,6 +48,7 @@ public class DatePatternFreqIndicatorImpl extends FrequencyIndicatorImpl impleme
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected DatePatternFreqIndicatorImpl() {
@@ -56,6 +57,7 @@ public class DatePatternFreqIndicatorImpl extends FrequencyIndicatorImpl impleme
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -261,11 +263,11 @@ public class DatePatternFreqIndicatorImpl extends FrequencyIndicatorImpl impleme
     @Override
     public void handleDrillDownData(Object masterObject, List<Object> inputRowList) {
         // Only for DelimitedFile,format a date from 'Thu Jan 01 00:00:00 CST 1970' to datePattern like as '1970-01-01'
-        Object data = masterObject;
-        if (!StringUtils.isEmpty(this.datePattern)) {
-            data = DateFormatUtils.format((Date) data, datePattern);
-        }
-        List<ModelMatcher> matchers = dateRetriever.findMatchers(String.valueOf(data));
+        // Object data = masterObject;
+        // if (!StringUtils.isEmpty(this.datePattern)) {
+        // data = DateFormatUtils.format((Date) data, datePattern);
+        // }
+        List<ModelMatcher> matchers = dateRetriever.findMatchers(String.valueOf(masterObject));
         for (ModelMatcher matcher : matchers) {
             drillDownMap = (DBMap<Object, List<Object>>) getMapDB(matcher.getModel());
             // check the size of limite
